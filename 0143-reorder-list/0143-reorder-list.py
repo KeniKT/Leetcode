72551c7739b91173d7471115ec639070aa9b3778
Kenean Tilahun <99726621+KeniKT@users.noreply.github.com>
2025-03-23 11:22:29 +0300

    Time: 3 ms (47.49%), Space: 23.3 MB (38.41%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,35 @@
+
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
+            return
+
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second_half = slow.next
+        slow.next = None
+
+        prev, curr = None, second_half
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        first_half, second_half = head, prev
+        while second_half:
+            first_next = first_half.next
+            second_next = second_half.next
+
+            first_half.next = second_half
+            second_half.next = first_next
+
+            first_half = first_next
+            second_half = second_next
